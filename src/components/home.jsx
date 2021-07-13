b1ea204d4bdb72d79618133a94d2f00f110fdc0c
Ronuel Moralita <moralitaronuel@gmail.com>
2021-07-13 11:38:54 +0800

    fifteen

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -65,7 +65,8 @@ export default class Home extends Component {
                 About me
               </h1>
               <p className="content-para">
-                I work as a Computer Programmer at the Municipal’s Civil Registrar Office of Real, Quezon. My duties are to design, develop, and test software. I am passionate about creating a more dynamic and user-friendly application, including a minimalist Graphical User Interface (GUI). During my spare time, I work on the frontend and backend of a website, desktop, and mobile application and learn new things about technology.
+                I work as a Computer Programmer at the Municipal Civil Registrar Office of Real, Quezon. My duties are to design, develop, and test software. I am passionate about creating a more dynamic and user-friendly application, including a minimalist Graphical User Interface (GUI). 
+                {/*During my spare time, I work on the frontend and backend of a website, desktop, and mobile application and learn new things about technology.*/}
                 </p>
             </div>
             <div className="content-container">
