e0965ca910e65220bfbc628ffae87a4579029efb
Ronuel Moralita <moralitaronuel@gmail.com>
2021-10-15 13:05:30 +0800

    fifteen

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ var n = weekday[d.getDay()];
 
 
 function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
     return (
       <div>
 
